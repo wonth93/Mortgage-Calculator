04634de239f49f153b65880357b9decd2ccc3928
wonth93 <wongth93@gmail.com>
2023-06-11 20:17:30 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Creating a mortgage Calculator!</p>
+        <p>Please wait...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
