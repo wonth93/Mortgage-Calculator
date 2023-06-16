91412d36539b13db8764c7a8106f04d86e26bab6
wonth93 <wongth93@gmail.com>
2023-06-15 17:01:58 -0700

    create a slider

--- src/Components/Common/SliderComponent.js
+++ src/Components/Common/SliderComponent.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { Slider, Typography, Stack } from "@mui/material";
 
-const SliderComponent = ({}) => {
+const SliderComponent = ({defaultValue, min, max, label, unit, onChange, amount, value, steps}) => {
   return (
-    <Stack>
-      
+    <Stack my={1.4}>
+      <Stack gap={1}>
+        <Typography variant="subtitle1">{/* {label} */} Something somthing value</Typography>
+        <Typography variant="h5">{/* {unit} {amount} */}$ x,xxx.yy</Typography>
+      </Stack>
+      <Slider 
+        defaultValue={defaultValue}
+        min={min}
+        max={max}
+        step={steps}
+        value={value}
+        onChange={onChange}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        marks
+      />
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="caption">{/* {unit} {min} */}$ x,xxx.yy</Typography>
+        <Typography variant="caption">{/* {unit} {max} */}$ x,xxx.yy</Typography>
+      </Stack>
     </Stack>
   )
 }
--- src/Components/SliderSelect.js
+++ src/Components/SliderSelect.js
@@ -1,11 +1,10 @@
 import React from "react";
+import SliderComponent from "./Common/SliderComponent";
 
 const SliderSelect = () => {
   return (
     <div>
-      <p>
-        This is SliderSelect
-      </p>
+      <SliderComponent />
     </div>
   );
 }
